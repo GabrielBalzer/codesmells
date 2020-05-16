8fdaa777051d68ee96befe64deaff03998ae791f
GabrielBalzer <gbalzer96@gmail.com>
2020-05-16 21:13:32 +0200

    Delete Vector and Enumeration

--- src/Customer.java
+++ src/Customer.java
@@ -1,16 +1,17 @@
-import java.util.Enumeration;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Vector;
 
 class Customer {
     private final String name;
-    private final Vector rentals = new Vector();
+    private final List<Rental> rentals = new ArrayList<>();
 
     public Customer(String newname) {
         name = newname;
     }
 
     public void addRental(Rental arg) {
-        rentals.addElement(arg);
+        rentals.add(arg);
     }
 
     public String getName() {
@@ -20,24 +21,28 @@ class Customer {
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
-        Enumeration enum_rentals = rentals.elements();
+
         String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
-        while (enum_rentals.hasMoreElements()) {
-            double thisAmount;
-            Rental each = (Rental) enum_rentals.nextElement();
+        for (Rental each: rentals) {
+            double thisAmount = 0;
+
             //determine amounts for each line
             thisAmount = each.amountFor();
+
             // add frequent renter points
-            frequentRenterPoints++;
+            frequentRenterPoints ++;
+
             // add bonus for a two day new release rental
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-                frequentRenterPoints++;
+                frequentRenterPoints ++;
+
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t" + thisAmount + "\n";
+            result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
             totalAmount += thisAmount;
         }
+
         //add footer lines
         result += "Amount owed is " + totalAmount + "\n";
         result += "You earned " + frequentRenterPoints + " frequent renter points";
